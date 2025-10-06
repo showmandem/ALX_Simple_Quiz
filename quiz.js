ee545fe5ff14cf6569aa75147c60b0667fd1fb3c
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-06 21:02:55 +0000

    trial

--- quiz.js
+++ quiz.js
@@ -2,7 +2,15 @@ document.addEventListener("DOMContentLoaded" , () => {
     function checkAnswer(){
         let correctAnswer = "4";
     }
-    document.querySelector("quiz")
+    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+
+        if (selectedOption) {
+    const userAnswer = selectedOption.value;
+    console.log("User selected:", userAnswer);
+}       else {
+    console.log("Please select an answer before submitting.");
+}
+
 
 
 
