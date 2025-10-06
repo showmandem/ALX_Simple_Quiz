c6f902a01f2fd6647b91549c2c2f971385ae78e0
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-06 17:09:09 +0000

    commit

---
+++ quiz.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded" , () => {
+    function checkAnswer(){
+        let correctAnswer = "4";
+    }
+    document.querySelector("quiz")
+
+
+
+
+
+
+
+
+
+})
