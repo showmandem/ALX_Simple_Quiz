8cdf093dbfd98b1119444a6a2d070429ea4d7920
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-06 21:32:54 +0000

    hi

--- quiz.js
+++ quiz.js
@@ -2,14 +2,14 @@ document.addEventListener("DOMContentLoaded" , () => {
     function checkAnswer(){
         let correctAnswer = "4";
     }
-    const selectedOption = document.querySelector('input[name="quiz"]:checked');
+    const userAnswer = document.querySelector('input[name="quiz"]:checked');
 
-        if (selectedOption) {
-    const userAnswer = selectedOption.value;
-    console.log("User selected:", userAnswer);
-}       else {
-    console.log("Please select an answer before submitting.");
-}
+//         if (userAnswer) {
+//     const userAnswer = selectedOption.value;
+//     console.log("User selected:", userAnswer);
+// }       else {
+//     console.log("Please select an answer before submitting.");
+// }
 
 if(userAnswer===correctAnswer){
     document.getElementById("feedback").innerHTML = "Correct Well done."
