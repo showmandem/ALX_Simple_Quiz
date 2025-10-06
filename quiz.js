fa113c4be3cc7752f82e4348d7d568e5e06e1e82
Derrick Owusu-Asare <aderrick649@gmail.com>
2025-10-06 21:17:54 +0000

    final push i guess

--- quiz.js
+++ quiz.js
@@ -4,17 +4,21 @@ document.addEventListener("DOMContentLoaded" , () => {
     }
     const selectedOption = document.querySelector('input[name="quiz"]:checked');
 
-        if (selectedOption) {
+//         if (selectedOption) {
     const userAnswer = selectedOption.value;
-    console.log("User selected:", userAnswer);
-}       else {
-    console.log("Please select an answer before submitting.");
+//     //console.log("User selected:", userAnswer);
+// }       else {
+//     console.log("Please select an answer before submitting.");
+// }
+
+if(userAnswer==correctAnswer){
+    document.getElementById("feedback").innerHTML = "Correct Well done."
 }
-
-
-
-
-
+else{
+    document.getElementById("feedback").innerHTML = "That's incorrect. Try again!"
+}
+const submitButton = document.getElementById("submit-answer")
+submitButton.addEventListener("click", checkAnswer)
 
 
 
